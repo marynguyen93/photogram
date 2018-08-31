efe6de5a0e5f2e3e14476c278957bf1d0a60389c
Mary Nguyen <maryn1993@gmail.com>
2018-08-31 13:04:32 -0700

    my first commit

---
+++ js/sidemenubar.js
@@ -0,0 +1,12 @@
+function openSM(){
+    document.getElementById("mySidemenu").style.width ="250px";
+    document.getElementById("pg-content").style.marginLeft = "250px";
+}
+function closeSM(){
+    document.getElementById("mySidemenu").style.width = "0";
+    document.getElementById("pg-content").style.marginLeft ="0";
+}
+
+
+
+
